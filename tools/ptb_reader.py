877d6a948238669a71ccbe01c5ee0088cc1a6630
csruiliu <rui.liu@gmx.com>
2021-04-01 17:37:19 -0500

    revise ptb reader

--- tools/ptb_reader.py
+++ tools/ptb_reader.py
@@ -44,24 +44,41 @@ def ptb_raw_data(data_path=None):
 
 
 def ptb_iterator(raw_data, batch_size, num_steps):
-    '''
-        ptb_iterator return the batch
-    '''
+    """Iterate on the raw PTB data.
+
+    This generates batch_size pointers into the raw PTB data,
+    and allows minibatch iteration along these pointers.
+
+    Args:
+        raw_data: one of the raw data outputs from ptb_raw_data.
+        batch_size: int, the batch size, the total number of training examples present in a single batch.
+        num_steps: int, the number of unrolls for models like LSTM.
+    Yields:
+        Pairs of the batched data, each a matrix of shape [batch_size, num_steps].
+        The second element of the tuple is the same data time-shifted to the
+        right by one.
+    Raises:
+        ValueError: if batch_size or num_steps are too high.
+    """
     raw_data = np.array(raw_data, dtype=np.int32)
+
     data_len = len(raw_data)
-    batch_len = data_len // batch_size
-    # create a new matrix with all zeros
-    data = np.zeros([batch_size, batch_len], dtype=np.int32)
+    # the number of batches
+    batch_num = data_len // batch_size
+
+    data = np.zeros([batch_size, batch_num], dtype=np.int32)
 
     for i in range(batch_size):
-        data[i] = raw_data[batch_len * i:batch_len * (i + 1)]
+        data[i] = raw_data[batch_num * i:batch_num * (i + 1)]
 
-    epoch_size = (batch_len - 1) // num_steps
+    # the second element is the same data time-shifted to the right by one, so "batch_num - 1"
+    # according to num of unrolled step, we get the number of iterations in a single epoch
+    epoch_size = (batch_num - 1) // num_steps
 
     if epoch_size == 0:
         raise ValueError("epoch_size == 0, decrease batch_size or num_steps")
 
     for i in range(epoch_size):
-        x = data[:, i*num_steps:(i+1)*num_steps]
-        y = data[:, i*num_steps+1:(i+1)*num_steps+1]
+        x = data[:, i * num_steps:(i + 1) * num_steps]
+        y = data[:, i * num_steps + 1:(i + 1) * num_steps + 1]
         yield x, y
