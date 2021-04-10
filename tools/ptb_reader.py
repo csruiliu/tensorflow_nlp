b3ac6872ef9d639f55b28ae3b6f3034788cc7d9b
csruiliu <rui.liu@gmx.com>
2021-04-09 21:20:25 -0500

    add skipgrams iterator

--- tools/ptb_reader.py
+++ tools/ptb_reader.py
@@ -29,10 +29,10 @@ def _file_to_word_ids(filename, word_to_id):
     return [word_to_id[word] for word in data]
 
 
-def ptb_raw_data(data_path=None):
-    train_path = os.path.join(data_path, "ptb.train.txt")
-    valid_path = os.path.join(data_path, "ptb.valid.txt")
-    test_path = os.path.join(data_path, "ptb.test.txt")
+def ptb_data_raw(folder_path):
+    train_path = os.path.join(folder_path, "ptb.train.txt")
+    valid_path = os.path.join(folder_path, "ptb.valid.txt")
+    test_path = os.path.join(folder_path, "ptb.test.txt")
 
     word_to_id = _build_vocab(train_path)
     train_data = _file_to_word_ids(train_path, word_to_id)
@@ -41,6 +41,46 @@ def ptb_raw_data(data_path=None):
     vocabulary_size = len(word_to_id)
 
     return train_data, valid_data, test_data, vocabulary_size
+
+
+def ptb_data_skipgram(folder_path, skip_grams=1):
+    train_path = os.path.join(folder_path, "ptb.train.txt")
+    valid_path = os.path.join(folder_path, "ptb.valid.txt")
+    test_path = os.path.join(folder_path, "ptb.test.txt")
+
+    train_word_sequence = _read_words(train_path)
+    valid_word_sequence = _read_words(valid_path)
+    test_word_sequence = _read_words(test_path)
+
+    word_to_id = _build_vocab(train_path)
+
+    vocabulary_size = len(word_to_id)
+
+    train_data_skipgrams = list()
+    valid_data_skipgrams = list()
+    test_data_skipgrams = list()
+    for i in range(skip_grams, vocabulary_size - skip_grams):
+        train_target = word_to_id[train_word_sequence[i]]
+        valid_target = word_to_id[valid_word_sequence[i]]
+        test_target = word_to_id[test_word_sequence[i]]
+
+        train_context = [word_to_id[train_word_sequence[i - skip_grams]],
+                         word_to_id[train_word_sequence[i + skip_grams]]]
+        valid_context = [word_to_id[valid_word_sequence[i - skip_grams]],
+                         word_to_id[valid_word_sequence[i + skip_grams]]]
+        test_context = [word_to_id[test_word_sequence[i - skip_grams]],
+                        word_to_id[test_word_sequence[i + skip_grams]]]
+
+        for ct in train_context:
+            train_data_skipgrams.append([train_target, ct])
+
+        for ct in valid_context:
+            valid_data_skipgrams.append([valid_target, ct])
+
+        for ct in test_context:
+            test_data_skipgrams.append([test_target, ct])
+
+    return train_data_skipgrams, valid_data_skipgrams, test_data_skipgrams, vocabulary_size
 
 
 def ptb_iterator(raw_data, batch_size, num_steps):
@@ -112,3 +152,10 @@ def ptb_batch_iterator(raw_data, batch_size, num_steps, vocab_size):
             y_one_hot[k, y[k]] = 1
 
         yield x_one_hot, y_one_hot
+
+
+def ptb_batch_iterator_skipgrams(raw_data, batch_size, vocab_size):
+    for i in np.arange(0, len(raw_data), batch_size):
+        random_input = np.eye(vocab_size)[raw_data[i][0]]
+        random_label = np.eye(vocab_size)[raw_data[i][1]]
+        yield random_input, random_label
