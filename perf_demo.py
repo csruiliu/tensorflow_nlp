58fa1a92b8b43e2fc6ca9a9bfb781a5a5cea2ca1
csruiliu <rui.liu@proton.me>
2021-04-11 20:44:02 -0500

    test models

--- perf_demo.py
+++ perf_demo.py
@@ -1,11 +1,14 @@
 import tensorflow as tf
 import argparse
 import sys
-import os
 import numpy as np
+import os
 
 from models.nnlm import NNLM
 from models.word2vec import Word2Vec
+from models.bi_lstm import BiLSTM
+from models.textrnn import TextRNN
+from models.transformer import Transformer
 
 import tools.ptb_reader as ptb_reader
 import tools.model_trainer as model_trainer
@@ -13,29 +16,14 @@ import tools.model_trainer as model_trainer
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('-m', '--model', action='store', type=str, default='nnlm',
-                        help='the name of nlp model')
-    parser.add_argument('-u', '--unroll_step', action='store', type=int, default=35,
-                        help='the num of unrolled step for a nlp model')
-    parser.add_argument('-b', '--batchsize', action='store', type=int, default=20,
-                        help='set the training batch size')
-    parser.add_argument('-o', '--opt', action='store', type=str,
-                        choices=['Adam', 'SGD', 'Adagrad', 'Momentum'],
-                        help='set the training optimizer')
-    parser.add_argument('-r', '--lr', action='store', type=float,
-                        help='set the learning rate')
-    parser.add_argument('-e', '--epoch', action='store', type=int,
-                        help='set the number of training epoch')
-
-    args = parser.parse_args()
-    nlp_model = args.model
-    num_steps = args.unroll_step
-    batch_size = args.batchsize
-    opt = args.opt
-    lr = args.lr
-    num_epoch = args.epoch
+    nlp_model = 'transformer'
+    num_steps = 35
+    batch_size = 20
+    opt = 'Adam'
+    lr = 0.0001
+    num_epoch = 1
+    num_hidden = 2
+    embedding_size = 2
 
     ptb_path = "/home/ruiliu/Development/tensorflow_ptb/dataset/"
 
@@ -47,9 +35,39 @@ if __name__ == "__main__":
         num_train_epochs = len(train_data)
         num_eval_epochs = len(eval_data)
 
-        model = NNLM(n_class=ptb_vocabulary_size, n_step=35, n_hidden=2)
+        model = NNLM(n_class=ptb_vocabulary_size, n_step=num_steps, n_hidden=num_hidden)
         feature_ph = tf.placeholder(tf.float32, [None, num_steps, ptb_vocabulary_size])
         label_ph = tf.placeholder(tf.float32, [None, ptb_vocabulary_size])
+        # build the model
+        logit = model.build(feature_ph)
+
+    elif nlp_model == 'bilstm':
+        ptb_train_data, ptb_valid_data, ptb_test_data, ptb_vocabulary_size = ptb_reader.ptb_data_raw(ptb_path)
+        train_data = list(ptb_reader.ptb_batch_iterator(ptb_train_data, batch_size, num_steps, ptb_vocabulary_size))
+        eval_data = list(ptb_reader.ptb_batch_iterator(ptb_valid_data, batch_size, num_steps, ptb_vocabulary_size))
+
+        num_train_epochs = len(train_data)
+        num_eval_epochs = len(eval_data)
+
+        model = BiLSTM(n_class=ptb_vocabulary_size, n_step=num_steps, n_hidden=num_hidden)
+        feature_ph = tf.placeholder(tf.float32, [None, num_steps, ptb_vocabulary_size])
+        label_ph = tf.placeholder(tf.float32, [None, ptb_vocabulary_size])
+        # build the model
+        logit = model.build(feature_ph)
+
+    elif nlp_model == 'textrnn':
+        ptb_train_data, ptb_valid_data, ptb_test_data, ptb_vocabulary_size = ptb_reader.ptb_data_raw(ptb_path)
+        train_data = list(ptb_reader.ptb_batch_iterator(ptb_train_data, batch_size, num_steps, ptb_vocabulary_size))
+        eval_data = list(ptb_reader.ptb_batch_iterator(ptb_valid_data, batch_size, num_steps, ptb_vocabulary_size))
+
+        num_train_epochs = len(train_data)
+        num_eval_epochs = len(eval_data)
+
+        model = TextRNN(n_class=ptb_vocabulary_size, n_step=num_steps, n_hidden=num_hidden)
+        feature_ph = tf.placeholder(tf.float32, [None, num_steps, ptb_vocabulary_size])
+        label_ph = tf.placeholder(tf.float32, [None, ptb_vocabulary_size])
+        # build the model
+        logit = model.build(feature_ph)
 
     elif nlp_model == 'word2vec':
         ptb_train_data, ptb_valid_data, ptb_test_data, ptb_vocabulary_size = ptb_reader.ptb_data_skipgram(ptb_path)
@@ -60,15 +78,15 @@ if __name__ == "__main__":
         num_train_epochs = len(train_data)
         num_eval_epochs = len(eval_data)
 
-        model = Word2Vec(voc_size=ptb_vocabulary_size, embedding_size=2)
+        model = Word2Vec(voc_size=ptb_vocabulary_size, embedding_size=embedding_size)
         feature_ph = tf.placeholder(tf.float32, shape=[None, ptb_vocabulary_size])
         label_ph = tf.placeholder(tf.float32, shape=[None, ptb_vocabulary_size])
+
+        logit = model.build(feature_ph)
 
     else:
         print('the model is not supported')
         sys.exit()
-
-    logit = model.build(feature_ph)
 
     # get the train operation
     train_op = model_trainer.train_model(logit, label_ph)
