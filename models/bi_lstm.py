ba8a0b2778c1ebe8bb19f1304c7940aec61b4b6e
csruiliu <rui.liu@gmx.com>
2021-06-07 10:31:25 -0500

    update

--- models/bi_lstm.py
+++ models/bi_lstm.py
@@ -1,32 +1,22 @@
-import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, InputLayer, Bidirectional, TimeDistributed, Embedding, Activation
+from keras.optimizers import Adam
 
 
 class BiLSTM:
-    def __init__(self, n_class, n_step=2, n_hidden=2):
-        self.num_class = n_class
-        self.num_step = n_step
-        self.num_hidden = n_hidden
+    def __init__(self, max_length):
+        self.max_length = max_length
 
-    def build(self, train_feature):
-        W = tf.Variable(tf.random_normal([self.num_hidden * 2, self.num_class]))
-        b = tf.Variable(tf.random_normal([self.num_class]))
+    def build(self, word2index, tag2index):
+        model = Sequential()
+        model.add(InputLayer(input_shape=(self.max_length,)))
+        model.add(Embedding(len(word2index), 128))
+        model.add(Bidirectional(LSTM(256, return_sequences=True)))
+        model.add(TimeDistributed(Dense(len(tag2index))))
+        model.add(Activation('softmax'))
 
-        lstm_fw_cell = tf.nn.rnn_cell.LSTMCell(self.num_hidden)
-        lstm_bw_cell = tf.nn.rnn_cell.LSTMCell(self.num_hidden)
-
-        # outputs : [batch_size, len_seq, n_hidden], states : [batch_size, n_hidden]
-        outputs, _ = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, train_feature, dtype=tf.float32)
-
-        # output[0] : lstm_fw, output[1] : lstm_bw
-        outputs = tf.concat([outputs[0], outputs[1]], 2)
-
-        # [n_step, batch_size, n_hidden]
-        outputs = tf.transpose(outputs, [1, 0, 2])
-
-        # [batch_size, n_hidden]
-        outputs = outputs[-1]
-
-        model = tf.matmul(outputs, W) + b
+        model.compile(loss='categorical_crossentropy',
+                      optimizer=Adam(0.001),
+                      metrics=['accuracy'])
 
         return model
-
---
+++ models/lstm.py
@@ -0,0 +1,23 @@
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, InputLayer, TimeDistributed, Embedding, Activation
+from keras.optimizers import Adam
+
+
+class LSTMNet:
+    def __init__(self, max_length):
+        self.max_length = max_length
+
+    def build(self, word2index, tag2index):
+        model = Sequential()
+        model.add(InputLayer(input_shape=(self.max_length,)))
+        model.add(Embedding(len(word2index), 128))
+        model.add(LSTM(256, return_sequences=True))
+        model.add(LSTM(256, return_sequences=True))
+        model.add(TimeDistributed(Dense(len(tag2index))))
+        model.add(Activation('softmax'))
+
+        model.compile(loss='categorical_crossentropy',
+                      optimizer=Adam(0.001),
+                      metrics=['accuracy'])
+
+        return model
