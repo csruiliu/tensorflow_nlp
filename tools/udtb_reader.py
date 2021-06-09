6be902f8f89f5683dc378c59a0f7c96f5df1ba0f
csruiliu <rui.liu@proton.me>
2021-06-09 10:32:35 -0500

    update bert demo

--- tools/udtb_reader.py
+++ tools/udtb_reader.py
@@ -29,3 +29,19 @@ def sentence_split(sentences, max_len):
         new_sentence.append(([data[x:x + max_len] for x in range(0, len(data), max_len)]))
     new_sentence = [val for sublist in new_sentence for val in sublist]
     return new_sentence
+
+
+def convert_ner_format(text, label, file):
+    with open(file, 'w') as f:
+        words = 0
+        i = 0
+        for zip_i in zip(text, label):
+            a, b = tuple(zip_i)
+            for r in range(len(a)):
+                item = a[r]+' '+b[r]
+                f.write("%s\n" % item)
+                words += 1
+            f.write("\n")
+            i += 1
+            #if i==3: break
+    print('Sentences:', i, 'Words:', words)
