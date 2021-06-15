fb16381b2eb77c664521d2a4555d2e743a3e0ae7
csruiliu <rui.liu@gmx.com>
2021-06-15 18:34:22 -0500

    refine udtb reader

--- lstm_demo.py
+++ lstm_demo.py
@@ -1,7 +1,4 @@
 import numpy as np
-import os
-from keras.preprocessing.sequence import pad_sequences
-import urllib
 
 from models.bi_lstm import BiLSTM
 from models.lstm import LSTMNet
@@ -20,96 +17,13 @@ def to_categorical(sequences, categories):
 
 
 if __name__ == "__main__":
-
-    ####################################################
-    # Download and load the dataset
-    ####################################################
-
-    UD_ENGLISH_TRAIN = './dataset/ud_treebank/en_partut-ud-train.conllu'
-    UD_ENGLISH_DEV = './dataset/ud_treebank/en_partut-ud-dev.conllu'
-    UD_ENGLISH_TEST = './dataset/ud_treebank/en_partut-ud-test.conllu'
-
-    if not os.path.exists(UD_ENGLISH_TRAIN):
-        urllib.request.urlretrieve('http://archive.aueb.gr:8085/files/en_partut-ud-train.conllu', UD_ENGLISH_TRAIN)
-    if not os.path.exists(UD_ENGLISH_DEV):
-        urllib.request.urlretrieve('http://archive.aueb.gr:8085/files/en_partut-ud-dev.conllu', UD_ENGLISH_DEV)
-    if not os.path.exists(UD_ENGLISH_TEST):
-        urllib.request.urlretrieve('http://archive.aueb.gr:8085/files/en_partut-ud-test.conllu', UD_ENGLISH_TEST)
-
-    train_sentences = udtb_reader.read_conllu(UD_ENGLISH_TRAIN)
-    val_sentences = udtb_reader.read_conllu(UD_ENGLISH_DEV)
-    test_sentences = udtb_reader.read_conllu(UD_ENGLISH_TEST)
-
-    ####################################################
-    # Preprocessing
-    ####################################################
-
-    train_text = udtb_reader.text_sequence(train_sentences)
-    test_text = udtb_reader.text_sequence(test_sentences)
-    # val_text = udtb_reader.text_sequence(val_sentences)
-
-    train_label = udtb_reader.tag_sequence(train_sentences)
-    test_label = udtb_reader.tag_sequence(test_sentences)
-    # val_label = udtb_reader.tag_sequence(val_sentences)
-
-    ####################################################
-    # Build dictionary with tag vocabulary
-    ####################################################
-
-    words, tags = set([]), set([])
-
-    for s in train_text:
-        for w in s:
-            words.add(w.lower())
-
-    for ts in train_label:
-        for t in ts:
-            tags.add(t)
-
-    word2index = {w: i + 2 for i, w in enumerate(list(words))}
-    # The special value used for padding
-    word2index['-PAD-'] = 0
-    # The special value used for OOVs
-    word2index['-OOV-'] = 1
-
-    tag2index = {t: i + 1 for i, t in enumerate(list(tags))}
-    # The special value used to padding
-    tag2index['-PAD-'] = 0
-
-    train_sentences_X, test_sentences_X, train_tags_y, test_tags_y = [], [], [], []
-
-    for s in train_text:
-        s_int = []
-        for w in s:
-            try:
-                s_int.append(word2index[w.lower()])
-            except KeyError:
-                s_int.append(word2index['-OOV-'])
-
-        train_sentences_X.append(s_int)
-
-    for s in test_text:
-        s_int = []
-        for w in s:
-            try:
-                s_int.append(word2index[w.lower()])
-            except KeyError:
-                s_int.append(word2index['-OOV-'])
-
-        test_sentences_X.append(s_int)
-
-    for s in train_label:
-        train_tags_y.append([tag2index[t] for t in s])
-
-    for s in test_label:
-        test_tags_y.append([tag2index[t] for t in s])
-
-    MAX_LENGTH = len(max(train_sentences_X, key=len))
-
-    train_sentences_X = pad_sequences(train_sentences_X, maxlen=MAX_LENGTH, padding='post')
-    test_sentences_X = pad_sequences(test_sentences_X, maxlen=MAX_LENGTH, padding='post')
-    train_tags_y = pad_sequences(train_tags_y, maxlen=MAX_LENGTH, padding='post')
-    test_tags_y = pad_sequences(test_tags_y, maxlen=MAX_LENGTH, padding='post')
+    (train_sentences_x,
+     val_sentences_x,
+     train_tags_y,
+     val_tags_y,
+     MAX_LENGTH,
+     word2index,
+     tag2index) = udtb_reader.load_udtb_dataset()
 
     ####################################################
     # Build model for BiLSTM
@@ -118,12 +32,12 @@ if __name__ == "__main__":
     model = BiLSTM(MAX_LENGTH, learn_rate=0.001, optimizer='Adagrad')
     logit, _ = model.build(word2index, tag2index)
 
-    logit.fit(train_sentences_X,
+    logit.fit(train_sentences_x,
               to_categorical(train_tags_y, len(tag2index)),
               batch_size=64,
               epochs=10,
               validation_split=0.2)
-    scores = logit.evaluate(test_sentences_X, to_categorical(test_tags_y, len(tag2index)))
+    scores = logit.evaluate(val_sentences_x, to_categorical(val_tags_y, len(tag2index)))
 
     print(f"{logit.metrics_names[1]}: {scores[1]}")
 
@@ -134,11 +48,11 @@ if __name__ == "__main__":
     model = LSTMNet(MAX_LENGTH, learn_rate=0.001, optimizer='Adagrad')
     logit, _ = model.build(word2index, tag2index)
 
-    logit.fit(train_sentences_X,
+    logit.fit(train_sentences_x,
               to_categorical(train_tags_y, len(tag2index)),
               batch_size=64,
               epochs=10,
               validation_split=0.2)
-    scores = logit.evaluate(test_sentences_X, to_categorical(test_tags_y, len(tag2index)))
+    scores = logit.evaluate(val_sentences_x, to_categorical(val_tags_y, len(tag2index)))
 
     print('{}: {}'.format(logit.metrics_names[1], scores[1]))
--- tools/udtb_reader.py
+++ tools/udtb_reader.py
@@ -1,4 +1,7 @@
 import pyconll
+import os
+import urllib
+from keras.preprocessing.sequence import pad_sequences
 
 
 def read_conllu(path):
@@ -45,3 +48,75 @@ def convert_ner_format(text, label, file):
             i += 1
             #if i==3: break
     print('Sentences:', i, 'Words:', words)
+
+
+def load_udtb_dataset():
+    # Download and load the dataset
+    UD_ENGLISH_TRAIN = './dataset/ud_treebank/en_partut-ud-train.conllu'
+    UD_ENGLISH_DEV = './dataset/ud_treebank/en_partut-ud-dev.conllu'
+    UD_ENGLISH_TEST = './dataset/ud_treebank/en_partut-ud-test.conllu'
+
+    if not os.path.exists(UD_ENGLISH_TRAIN):
+        urllib.request.urlretrieve('http://archive.aueb.gr:8085/files/en_partut-ud-train.conllu', UD_ENGLISH_TRAIN)
+    if not os.path.exists(UD_ENGLISH_DEV):
+        urllib.request.urlretrieve('http://archive.aueb.gr:8085/files/en_partut-ud-dev.conllu', UD_ENGLISH_DEV)
+    if not os.path.exists(UD_ENGLISH_TEST):
+        urllib.request.urlretrieve('http://archive.aueb.gr:8085/files/en_partut-ud-test.conllu', UD_ENGLISH_TEST)
+
+    train_sentences = read_conllu(UD_ENGLISH_TRAIN)
+    val_sentences = read_conllu(UD_ENGLISH_DEV)
+
+    # read the train and eval text
+    train_text = text_sequence(train_sentences)
+    val_text = text_sequence(val_sentences)
+
+    train_label = tag_sequence(train_sentences)
+    val_label = tag_sequence(val_sentences)
+
+    # build dictionary with tag vocabulary
+    words, tags = set([]), set([])
+    for s in train_text:
+        for w in s:
+            words.add(w.lower())
+    for ts in train_label:
+        for t in ts:
+            tags.add(t)
+    word2index = {w: i + 2 for i, w in enumerate(list(words))}
+    word2index['-PAD-'] = 0
+    word2index['-OOV-'] = 1
+    tag2index = {t: i + 1 for i, t in enumerate(list(tags))}
+    tag2index['-PAD-'] = 0
+
+    # prepare the training data
+    train_sentences_x, val_sentences_x, train_tags_y, val_tags_y = [], [], [], []
+    for s in train_text:
+        s_int = []
+        for w in s:
+            try:
+                s_int.append(word2index[w.lower()])
+            except KeyError:
+                s_int.append(word2index['-OOV-'])
+        train_sentences_x.append(s_int)
+
+    for s in val_text:
+        s_int = []
+        for w in s:
+            try:
+                s_int.append(word2index[w.lower()])
+            except KeyError:
+                s_int.append(word2index['-OOV-'])
+        val_sentences_x.append(s_int)
+
+    for s in train_label:
+        train_tags_y.append([tag2index[t] for t in s])
+    for s in val_label:
+        val_tags_y.append([tag2index[t] for t in s])
+
+    MAX_LENGTH = len(max(train_sentences_x, key=len))
+
+    train_sentences_x = pad_sequences(train_sentences_x, maxlen=MAX_LENGTH, padding='post')
+    val_sentences_x = pad_sequences(val_sentences_x, maxlen=MAX_LENGTH, padding='post')
+    train_tags_y = pad_sequences(train_tags_y, maxlen=MAX_LENGTH, padding='post')
+    val_tags_y = pad_sequences(val_tags_y, maxlen=MAX_LENGTH, padding='post')
+
+    return train_sentences_x, val_sentences_x, train_tags_y, val_tags_y, MAX_LENGTH, word2index, tag2index
