4f5bc3a7fceb7b7f0dc6f8ba912b293d13abf625
csruiliu <rui.liu@gmx.com>
2021-06-15 18:40:22 -0500

    add lmrd_reader

---
+++ tools/lmrd_reader.py
@@ -0,0 +1,162 @@
+import os
+import re
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import tensorflow_hub as hub
+from bert.tokenization import FullTokenizer
+
+
+# Load all files from a directory in a DataFrame.
+def load_directory_data(directory):
+    data = {}
+    data["sentence"] = []
+    data["sentiment"] = []
+    for file_path in os.listdir(directory):
+        with tf.gfile.GFile(os.path.join(directory, file_path), "r") as f:
+            data["sentence"].append(f.read())
+            data["sentiment"].append(re.match("\d+_(\d+)\.txt", file_path).group(1))
+    return pd.DataFrame.from_dict(data)
+
+
+# Merge positive and negative examples, add a polarity column and shuffle.
+def load_dataset(directory):
+    pos_df = load_directory_data(os.path.join(directory, "pos"))
+    neg_df = load_directory_data(os.path.join(directory, "neg"))
+    pos_df["polarity"] = 1
+    neg_df["polarity"] = 0
+    return pd.concat([pos_df, neg_df]).sample(frac=1).reset_index(drop=True)
+
+
+# Download and process the dataset files.
+def download_and_load_datasets(force_download=False):
+    if not os.path.exists('/tank/local/ruiliu/datasets/aclImdb.tar.gz'):
+        dataset = tf.keras.utils.get_file(
+            fname="aclImdb.tar.gz",
+            origin="http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz",
+            extract=True,
+            cache_dir='/tank/local/ruiliu/'
+        )
+    else:
+        dataset = '/tank/local/ruiliu/datasets/aclImdb.tar.gz'
+
+    train_df = load_dataset(os.path.join(os.path.dirname(dataset), "aclImdb", "train"))
+    test_df = load_dataset(os.path.join(os.path.dirname(dataset), "aclImdb", "test"))
+
+    return train_df, test_df
+
+
+class PaddingInputExample(object):
+    """Fake example so the num input examples is a multiple of the batch size.
+    When running eval/predict on the TPU, we need to pad the number of examples
+    to be a multiple of the batch size, because the TPU requires a fixed batch
+    size. The alternative is to drop the last batch, which is bad because it means
+    the entire output data won't be generated.
+    We use this class instead of `None` because treating `None` as padding
+    batches could cause silent errors.
+    """
+
+
+class InputExample(object):
+    """A single training/test example for simple sequence classification."""
+
+    def __init__(self, guid, text_a, text_b=None, label=None):
+        """Constructs a InputExample.
+            Args:
+            guid: Unique id for the example.
+            text_a: string. The untokenized text of the first sequence.
+                    For single sequence tasks, only this sequence must be specified.
+            text_b: (Optional) string. The untokenized text of the second sequence.
+                    Only must be specified for sequence pair tasks.
+            label: (Optional) string. The label of the example.
+                   This should be specified for train and dev examples, but not for test examples.
+        """
+        self.guid = guid
+        self.text_a = text_a
+        self.text_b = text_b
+        self.label = label
+
+
+def create_tokenizer_from_hub_module(bert_path, sess):
+    """Get the vocab file and casing info from the Hub module."""
+    bert_module = hub.Module(bert_path)
+    tokenization_info = bert_module(signature="tokenization_info", as_dict=True)
+    vocab_file, do_lower_case = sess.run(
+        [tokenization_info["vocab_file"], tokenization_info["do_lower_case"]]
+    )
+
+    return FullTokenizer(vocab_file=vocab_file, do_lower_case=do_lower_case)
+
+
+def convert_single_example(tokenizer, example, max_seq_length=256):
+    """Converts a single `InputExample` into a single `InputFeatures`."""
+
+    if isinstance(example, PaddingInputExample):
+        input_ids = [0] * max_seq_length
+        input_mask = [0] * max_seq_length
+        segment_ids = [0] * max_seq_length
+        label = 0
+        return input_ids, input_mask, segment_ids, label
+
+    tokens_a = tokenizer.tokenize(example.text_a)
+    if len(tokens_a) > max_seq_length - 2:
+        tokens_a = tokens_a[0: (max_seq_length - 2)]
+
+    tokens = []
+    segment_ids = []
+    tokens.append("[CLS]")
+    segment_ids.append(0)
+    for token in tokens_a:
+        tokens.append(token)
+        segment_ids.append(0)
+    tokens.append("[SEP]")
+    segment_ids.append(0)
+
+    input_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+    # The mask has 1 for real tokens and 0 for padding tokens. Only real
+    # tokens are attended to.
+    input_mask = [1] * len(input_ids)
+
+    # Zero-pad up to the sequence length.
+    while len(input_ids) < max_seq_length:
+        input_ids.append(0)
+        input_mask.append(0)
+        segment_ids.append(0)
+
+    assert len(input_ids) == max_seq_length
+    assert len(input_mask) == max_seq_length
+    assert len(segment_ids) == max_seq_length
+
+    return input_ids, input_mask, segment_ids, example.label
+
+
+def convert_examples_to_features(tokenizer, examples, max_seq_length=256):
+    """Convert a set of `InputExample`s to a list of `InputFeatures`."""
+
+    input_ids, input_masks, segment_ids, labels = [], [], [], []
+    # for example in tqdm(examples, desc="Converting examples to features"):
+    for example in examples:
+        input_id, input_mask, segment_id, label = convert_single_example(
+            tokenizer, example, max_seq_length
+        )
+        input_ids.append(input_id)
+        input_masks.append(input_mask)
+        segment_ids.append(segment_id)
+        labels.append(label)
+    return (
+        np.array(input_ids),
+        np.array(input_masks),
+        np.array(segment_ids),
+        np.array(labels).reshape(-1, 1),
+    )
+
+
+def convert_text_to_examples(texts, labels):
+    """Create InputExamples"""
+    InputExamples = []
+    for text, label in zip(texts, labels):
+        InputExamples.append(
+            InputExample(guid=None, text_a=" ".join(text), text_b=None, label=label)
+        )
+    return InputExamples
