62d1b655d141d223fe558cca6afbae50f615a20f
csruiliu <rui.liu@gmx.com>
2021-06-15 19:02:38 -0500

    refine udtb reader

--- lstm_demo.py
+++ lstm_demo.py
@@ -5,17 +5,6 @@ from models.lstm import LSTMNet
 import tools.udtb_reader as udtb_reader
 
 
-def to_categorical(sequences, categories):
-    cat_sequences = []
-    for s in sequences:
-        cats = []
-        for item in s:
-            cats.append(np.zeros(categories))
-            cats[-1][item] = 1.0
-        cat_sequences.append(cats)
-    return np.array(cat_sequences)
-
-
 if __name__ == "__main__":
     (train_sentences_x,
      val_sentences_x,
@@ -33,11 +22,11 @@ if __name__ == "__main__":
     logit, _ = model.build(word2index, tag2index)
 
     logit.fit(train_sentences_x,
-              to_categorical(train_tags_y, len(tag2index)),
+              udtb_reader.to_categorical(train_tags_y, len(tag2index)),
               batch_size=64,
               epochs=10,
               validation_split=0.2)
-    scores = logit.evaluate(val_sentences_x, to_categorical(val_tags_y, len(tag2index)))
+    scores = logit.evaluate(val_sentences_x, udtb_reader.to_categorical(val_tags_y, len(tag2index)))
 
     print(f"{logit.metrics_names[1]}: {scores[1]}")
 
@@ -53,6 +42,6 @@ if __name__ == "__main__":
               batch_size=64,
               epochs=10,
               validation_split=0.2)
-    scores = logit.evaluate(val_sentences_x, to_categorical(val_tags_y, len(tag2index)))
+    scores = logit.evaluate(val_sentences_x, udtb_reader.to_categorical(val_tags_y, len(tag2index)))
 
     print('{}: {}'.format(logit.metrics_names[1], scores[1]))
