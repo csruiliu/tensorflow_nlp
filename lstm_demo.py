9f74f181d449bd844db5ee8e124bf4f7ca7d2d10
csruiliu <rui.liu@proton.me>
2021-06-17 11:56:00 -0500

    add model save and load

--- lstm_demo.py
+++ lstm_demo.py
@@ -1,6 +1,7 @@
 from models.bi_lstm import BiLSTM
 from models.lstm import LSTMNet
 import tools.udtb_reader as udtb_reader
+import tensorflow as tf
 
 
 if __name__ == "__main__":
@@ -16,30 +17,67 @@ if __name__ == "__main__":
     # Build model for BiLSTM
     ####################################################
 
-    model = BiLSTM(MAX_LENGTH, learn_rate=0.001, optimizer='Adagrad')
-    logit, _ = model.build(word2index, tag2index)
+    config = tf.ConfigProto()
+    config.allow_soft_placement = True
+    config.gpu_options.allow_growth = True
 
-    logit.fit(train_sentences_x,
-              udtb_reader.to_categorical(train_tags_y, len(tag2index)),
-              batch_size=64,
-              epochs=10,
-              validation_split=0.2)
-    scores = logit.evaluate(val_sentences_x, udtb_reader.to_categorical(val_tags_y, len(tag2index)))
+    with tf.Session(config=config) as sess:
+        sess.run(tf.global_variables_initializer())
+        model = BiLSTM(MAX_LENGTH, learn_rate=0.001, optimizer='Adam')
+        logit, _ = model.build(word2index, tag2index)
 
-    print(f"{logit.metrics_names[1]}: {scores[1]}")
+        logit.fit(train_sentences_x,
+                  udtb_reader.to_categorical(train_tags_y, len(tag2index)),
+                  batch_size=64,
+                  epochs=1,
+                  validation_split=0.2)
+        scores = logit.evaluate(val_sentences_x, udtb_reader.to_categorical(val_tags_y, len(tag2index)))
+        print(f"{logit.metrics_names[1]}: {scores[1]}")
+
+        logit.save('lstm.h5')
+
+        logit.load_weights('lstm.h5')
+
+        logit.fit(train_sentences_x,
+                  udtb_reader.to_categorical(train_tags_y, len(tag2index)),
+                  batch_size=64,
+                  epochs=1,
+                  validation_split=0.2)
+
+        scores = logit.evaluate(val_sentences_x, udtb_reader.to_categorical(val_tags_y, len(tag2index)))
+        print(f"{logit.metrics_names[1]}: {scores[1]}")
 
     ####################################################
     # Build model for LSTM
     ####################################################
 
-    model = LSTMNet(MAX_LENGTH, learn_rate=0.001, optimizer='Adagrad')
-    logit, _ = model.build(word2index, tag2index)
+    config = tf.ConfigProto()
+    config.allow_soft_placement = True
+    config.gpu_options.allow_growth = True
 
-    logit.fit(train_sentences_x,
-              udtb_reader.to_categorical(train_tags_y, len(tag2index)),
-              batch_size=64,
-              epochs=10,
-              validation_split=0.2)
-    scores = logit.evaluate(val_sentences_x, udtb_reader.to_categorical(val_tags_y, len(tag2index)))
+    with tf.Session(config=config) as sess:
 
-    print('{}: {}'.format(logit.metrics_names[1], scores[1]))
+        model = LSTMNet(MAX_LENGTH, learn_rate=0.001, optimizer='Adagrad')
+        logit, _ = model.build(word2index, tag2index)
+
+        logit.fit(train_sentences_x,
+                  udtb_reader.to_categorical(train_tags_y, len(tag2index)),
+                  batch_size=64,
+                  epochs=10,
+                  validation_split=0.2)
+        scores = logit.evaluate(val_sentences_x, udtb_reader.to_categorical(val_tags_y, len(tag2index)))
+
+        print('{}: {}'.format(logit.metrics_names[1], scores[1]))
+
+        logit.save('bilstm.h5')
+
+        logit.load_weights('bilstm.h5')
+
+        logit.fit(train_sentences_x,
+                  udtb_reader.to_categorical(train_tags_y, len(tag2index)),
+                  batch_size=64,
+                  epochs=1,
+                  validation_split=0.2)
+
+        scores = logit.evaluate(val_sentences_x, udtb_reader.to_categorical(val_tags_y, len(tag2index)))
+        print(f"{logit.metrics_names[1]}: {scores[1]}")
