dbeaa57a250200c20d84a3fb4c61e0fc382a2748
csruiliu <rui.liu@proton.me>
2021-06-29 18:09:50 -0500

    switch from keras to tf

--- models/bi_lstm.py
+++ models/bi_lstm.py
@@ -1,9 +1,5 @@
-from keras.models import Sequential
-from keras.layers import Dense, LSTM, InputLayer, Bidirectional, TimeDistributed, Embedding, Activation
-from keras.optimizers import Adam
-from keras.optimizers import SGD
-from keras.optimizers import Adagrad
-from keras import backend as K
+from tensorflow import keras
+from tensorflow.keras import layers
 import numpy as np
 
 
@@ -11,28 +7,28 @@ class BiLSTM:
     def __init__(self, max_length, learn_rate=0.01, optimizer='Adam'):
         self.max_length = max_length
         if optimizer == 'Adam':
-            self.opt = Adam(lr=learn_rate)
+            self.opt = keras.optimizers.Adam(learning_rate=learn_rate)
         elif optimizer == 'SGD':
-            self.opt = SGD(lr=learn_rate)
+            self.opt = keras.optimizers.Adam(learning_rate=learn_rate)
         elif optimizer == 'Adagrad':
-            self.opt = Adagrad(lr=learn_rate)
+            self.opt = keras.optimizers.Adagrad(learning_rate=learn_rate)
         elif optimizer == 'Momentum':
-            self.opt = SGD(lr=learn_rate, decay=1e-6, momentum=0.9)
+            self.opt = keras.optimizers.SGD(learning_rate=learn_rate, decay=1e-6, momentum=0.9)
         else:
             raise ValueError('Optimizer is not recognized')
 
     def build(self, word2index, tag2index):
-        model = Sequential()
-        model.add(InputLayer(input_shape=(self.max_length,)))
-        model.add(Embedding(len(word2index), 128))
-        model.add(Bidirectional(LSTM(256, return_sequences=True)))
-        model.add(TimeDistributed(Dense(len(tag2index))))
-        model.add(Activation('softmax'))
-
-        trainable_count = int(np.sum([K.count_params(p) for p in set(model.trainable_weights)]))
+        model = keras.Sequential()
+        model.add(layers.InputLayer(input_shape=(self.max_length,)))
+        model.add(layers.Embedding(len(word2index), 128))
+        model.add(layers.Bidirectional(layers.LSTM(256, return_sequences=True)))
+        model.add(layers.TimeDistributed(layers.Dense(len(tag2index))))
+        model.add(layers.Activation('softmax'))
 
         model.compile(loss='categorical_crossentropy',
                       optimizer=self.opt,
                       metrics=['accuracy'])
+
+        trainable_count = int(np.sum([keras.backend.count_params(p) for p in set(model.trainable_weights)]))
 
         return model, trainable_count
