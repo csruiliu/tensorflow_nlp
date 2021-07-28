23cfd6456ca4119b1f20f33708cb7e9a76e6ece9
csruiliu <rui.liu@gmx.com>
2021-07-27 19:57:22 -0500

    add dc reader for text8

---
+++ tools/dc_reader.py
@@ -0,0 +1,119 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import zipfile
+
+import tensorflow as tf
+import numpy as np
+import os
+import urllib
+import random
+from pathlib import Path
+import collections
+
+'''
+Data Compression Programs Dataset by Matt Mahoney
+http://mattmahoney.net/dc/
+'''
+
+dc_url = 'http://mattmahoney.net/dc/'
+
+
+def download_dataset(file_name, expected_bytes):
+    dc_dataset = Path('./dataset/' + file_name)
+    if not dc_dataset.exists():
+        dc_file, _ = urllib.request.urlretrieve(url=dc_url + file_name, filename=dc_dataset)
+    else:
+        dc_file = dc_dataset
+    statinfo = os.stat(dc_file)
+    if statinfo.st_size == expected_bytes:
+        print('Found and verified: {}'.format(file_name))
+    else:
+        print(statinfo.st_size)
+        raise Exception('Failed to verify {}'.format(file_name))
+    return dc_file
+
+
+def load_data(file_name):
+    with zipfile.ZipFile(file_name) as f:
+        data = tf.compat.as_str(f.read(f.namelist()[0])).split()
+    return data
+
+
+def build_dataset(words, voc_size):
+    # build the vocabulary, count the appearance of each word and return the top #voc_size
+    count = [['UNK', -1]]
+    count.extend(collections.Counter(words).most_common(voc_size - 1))
+    dictionary = dict()
+    # convert word to index, only focus on top #voc_size, so set the other
+    for word, _ in count:
+        # e.g., {'the': 1, 'UNK': 0, 'a': 12}
+        dictionary[word] = len(dictionary)
+    data = list()
+    # count for word other than top #voc_size
+    unk_count = 0
+    for word in words:
+        if word in dictionary:
+            index = dictionary[word]
+        else:
+            index = 0
+            unk_count += 1
+        data.append(index)
+
+    count[0][1] = unk_count
+    reverse_dict = dict(zip(dictionary.values(),dictionary.keys()))
+
+    return data, count, dictionary, reverse_dict
+
+
+def generate_batch(data, batch_size, num_skips, skip_window):
+    '''
+    Args:
+        batch_size: number of training batch
+        num_skips: sample size of each word
+        skip_window: the distance for a word can be consider
+    Returns:
+        every sample in a batch and the associate
+    '''
+
+    global data_index
+    assert batch_size % num_skips == 0
+    assert num_skips <= skip_window * 2
+
+    batch = np.ndarray(shape=(batch_size,), dtype=np.int32)
+    labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
+    span = 2 * skip_window + 1
+    buffer = collections.deque(maxlen=span)
+
+    for _ in range(span):
+        buffer.append(data[data_index])
+        data_index = (data_index + 1) % len(data)
+
+    for i in range(batch_size // num_skips):
+        target = skip_window
+        targets_avoid = [skip_window]
+        for j in range(num_skips):
+
+            while target in targets_avoid:
+                target = random.randint(0, span - 1)
+            targets_avoid.append(target)
+            batch[i * num_skips + j] = buffer[skip_window]
+            labels[i * num_skips + j, 0] = buffer[target]
+
+        buffer.append(data[data_index])
+        data_index = (data_index + 1) % len(data)
+    return batch, labels
+
+
+if __name__ == "__main__":
+    data_index = 0
+
+    text8_file = download_dataset('text8.zip', 31344016)
+    text8_words = load_data(text8_file)
+
+    print('Data size', len(text8_words))
+    text8_data, text8_count, text8_dict, text8_reverse_dict = build_dataset(text8_words, 50000)
+    del text8_words
+    batch, labels = generate_batch(text8_data, 8, 2, 1)
+    print(batch)
