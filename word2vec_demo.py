3caf425094f8dc6e6612dd5c708eaad7d381b00b
csruiliu <rui.liu@proton.me>
2021-07-28 18:45:54 -0500

    update word2vec demo

--- word2vec_demo.py
+++ word2vec_demo.py
@@ -1,7 +1,6 @@
 import numpy as np
 import tensorflow as tf
 from tools.dc_reader import download_dataset, load_data, build_dataset, generate_batch
-from tools.model_trainer import train_model, eval_model
 from models.word2vec import Word2Vec
 
 
@@ -24,7 +23,7 @@ def main():
     dc_url = 'http://mattmahoney.net/dc/'
     text8_file = download_dataset(dc_url, 'text8.zip', 31344016)
     text8_words = load_data(text8_file)
-    text8_data, text8_count, text8_dict, text8_reverse_dict = build_dataset(text8_words, 50000)
+    text8_data, text8_count, text8_dict, text8_reverse_dict = build_dataset(text8_words, voc_size)
     del text8_words
 
     ####################################################
@@ -37,16 +36,21 @@ def main():
     label_ph = tf.placeholder(tf.int32, [batch_size, 1])
     eval_dataset = tf.constant(valid_examples, tf.int32)
 
-    logit = model.build(feature_ph)
-    train_op = train_model(logit, label_ph, opt='SGD', lr=0.1)
-    eval_op = eval_model(logit, label_ph)
+    embeddings, embed, nce_weights, nce_biases = model.build(feature_ph)
 
-    embeddings = tf.Variable(tf.random_uniform([voc_size, embedding_size], -1.0, 1.0))
+    loss = tf.reduce_mean(tf.nn.nce_loss(weights=nce_weights,
+                                         biases=nce_biases,
+                                         labels=label_ph,
+                                         inputs=embed,
+                                         num_sampled=num_sampled,
+                                         num_classes=voc_size))
+
+    train_op = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
     norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
     normalized_embeddings = embeddings / norm
-    eval_embeddings = tf.nn.embedding_lookup(normalized_embeddings, eval_dataset)
-    similarity = tf.matmul(eval_embeddings, normalized_embeddings, transpose_b=True)
+    valid_embeddings = tf.nn.embedding_lookup(normalized_embeddings, eval_dataset)
+    similarity = tf.matmul(valid_embeddings, normalized_embeddings, transpose_b=True)
 
     config = tf.ConfigProto()
     config.allow_soft_placement = True
@@ -54,7 +58,6 @@ def main():
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        average_loss = 0
 
         # count the parameters for the model
         total_parameters = 0
@@ -64,29 +67,32 @@ def main():
             for dim in shape:
                 variable_parameters *= dim.value
             total_parameters += variable_parameters
+        print('Total training parameters: {}'.format(total_parameters))
 
+        average_loss = 0
         for step in range(num_steps):
             batch_inputs, batch_labels = generate_batch(text8_data, batch_size, num_skips, skip_window)
-            sess.run(train_op, feed_dict={feature_ph: batch_inputs, label_ph: batch_labels})
-            step_loss = sess.run(eval_op, feed_dict={feature_ph: batch_inputs, label_ph: batch_labels})
-            average_loss += step_loss
+            # print('Training step: {} / {}'.format(step, num_steps))
+            _, train_loss = sess.run([train_op, loss], feed_dict={feature_ph: batch_inputs, label_ph: batch_labels})
+            average_loss += train_loss
 
-            if step + 1 % 1999 == 0:
+            if (step + 1) % 2000 == 0:
                 average_loss /= 2000
-                print("Average loss for step {}: {}".format(step, average_loss))
+                print("train loss for step {}:{}".format(step, average_loss))
                 average_loss = 0
 
-        sim = similarity.eval()
-        for i in range(valid_size):
-            valid_word = text8_reverse_dict[valid_examples[i]]
-            top_k = 8
-            nearest = (-sim[i, :]).argsort()[1:top_k + 1]
-            log_str = "most similar word: ".format(str(valid_word))
+            if (step + 1) % 10000 == 0:
+                sim = similarity.eval()
+                for i in range(valid_size):
+                    valid_word = text8_reverse_dict[valid_examples[i]]
+                    top_k = 8
+                    nearest = (-sim[i, :]).argsort()[1:top_k + 1]
+                    log_str = "Most similar word of {}: ".format(str(valid_word))
 
-            for k in range(top_k):
-                close_word = text8_reverse_dict[nearest[k]]
-                log_str = "%s %s, " % (log_str, close_word)
-            print(log_str)
+                    for k in range(top_k):
+                        close_word = text8_reverse_dict[nearest[k]]
+                        log_str = "%s %s, " % (log_str, close_word)
+                    print(log_str)
 
 
 if __name__ == "__main__":
